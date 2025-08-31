61b94b98ddcbf50e24b3fbedfe61a6d969282b16
meremyd <128643046+meremyd@users.noreply.github.com>
2025-08-31 22:41:24 +0800

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,15 +5,14 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
-// Connect to MongoDB
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('✅ Connected to MongoDB');
+    console.log('Connected to MongoDB');
     app.listen(PORT, () => {
-      console.log(`🚀 Server running on http://localhost:${PORT}`);
+      console.log(`Server running on http://localhost:${PORT}`);
     });
   })
   .catch((err) => {
-    console.error('❌ MongoDB connection error:', err);
+    console.error('MongoDB connection error:', err);
   });
